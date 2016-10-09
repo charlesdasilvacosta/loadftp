3fa7a2d4c167f93b7ba8ef093d1f363a86f8d968
charlesdasilvacosta <dasilvacosta.charles@gmail.com>
2016-10-09 21:43:06 +0200

    ajout du readme et du programme

---
+++ README.md
@@ -0,0 +1,22 @@
+##load_ftp
+
+This python script permits to connect to a server FTP and download all the files.
+The file tree is respected.
+
+The files are placed in a directory "data".
+
+###Getting Started
+
+To run, in the terminal:
+
+<pre><code>python load_ftp.py</code></pre>
+
+Enter the informations requested.
+
+
+
+
+
+###Authors
+
+Charles Da Silva Costa
---
+++ load_ftp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+import os
+import getpass
+from ftplib import FTP
+
+
+def downloadFile(connect, chemin):
+	files= []
+	connect.retrlines('LIST '+chemin,files.append)
+
+	for i in files:
+		if i.split()[8]!="." and i.split()[8]!="..":
+			print "Download of "+i.split()[8]
+			try:
+				if i[0]!='d':
+					connect.retrbinary('RETR '+chemin+'/'+i.split()[8], open("./data/"+chemin+'/'+i.split()[8], 'wb').write)
+				else : 
+					if not os.path.exists("./data/"+chemin+'/'+i.split()[8]):
+						os.makedirs("./data/"+chemin+'/'+i.split()[8])
+					downloadFile(connect,chemin+'/'+i.split()[8])
+			except:
+				print "Error with the file "+i.split()[8]
+	
+
+hote=raw_input("Enter the host : ")
+ident=raw_input("Enter the login : ")
+mdp=getpass.getpass("Enter the password : ")
+
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+try:
+	connect = FTP(hote, ident, mdp) 
+
+	downloadFile(connect,"./")
+	print "\nDownload finished\nDisconnection from the server"
+	connect.quit() 
+except:
+	print "Error in the informations"
